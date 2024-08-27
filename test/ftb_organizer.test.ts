368cd58766db981d9c6e5c366bdbe7f765cb2fa8
francescopanico <frpanico96@gmail.com>
2024-08-27 21:52:58 +0200

    Initial commit

---
+++ test/ftb_organizer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FtbOrganizer from '../lib/ftb_organizer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ftb_organizer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FtbOrganizer.FtbOrganizerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
